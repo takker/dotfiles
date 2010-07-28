d0987dbe0ed3120806018b384aab078ee397cd04
KASHIO Takaaki <t.kashio@gmail.com>
2010-07-28 23:12:46 +0900

    [Emacs] いくつかの関数の追加と削除
    * C-z => 次に指定した１文字に移動
    * M-z => 次に指定した文字まで削除(zap-to-charの改良版)
    * my-beginning-of-line, my-end-of-lineを削除(sequential-commandで十分なため)なため

--- .emacs.d/inits/01_util.el
+++ .emacs.d/inits/01_util.el
@@ -1,22 +1,19 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil; -*-
-;; C-x f: describe-face-at-point
-; カーソル位置のフェイスを調べる
+;;* C-x f => カーソル位置のフェイスを調べる
 (defun describe-face-at-point ()
   "Return face used at point."
   (interactive)
   (message "%s" (get-char-property (point) 'face)))
 (global-set-key [(C x)(f)] 'describe-face-at-point)
 
-;; C-c d: my-insert-date
-; 日付挿入
+;; C-c d => 日付挿入
 (defun my-insert-date ()
   (interactive)
   (insert (format-time-string "%Y-%m-%dT%R:%S+09:00" (current-time))))
 (global-set-key [(C c)(d)] 'my-insert-date)
 
-;; C-o: other-window-or-split
-; 他のウィンドウに移動(なければ分割)
-; http://d.hatena.ne.jp/rubikitch/20100210
+;;* C-o => 他のウィンドウに移動(なければ分割)
+;;** http://d.hatena.ne.jp/rubikitch/20100210
 (defun other-window-or-split ()
   (interactive)
   (when (one-window-p)
@@ -24,40 +21,20 @@
   (other-window 1))
 (global-set-key [(C o)] 'other-window-or-split)
 
-;; M-n: scroll-nlines-ahead
-;; M-p: scroll-nlines-behind
+;;* M-n => n行下にスクロール
+;;* M-p => n行上にスクロール
 (defalias 'scroll-ahead 'scroll-up)
 (defalias 'scroll-behind 'scroll-down)
-; n行下にスクロール
 (defun scroll-nlines-ahead (&optional n)
   "Scroll ahead N lines (1 by default)."
   (interactive "P")
   (scroll-ahead (prefix-numeric-value n)))
-; n行上にスクロール
 (defun scroll-nlines-behind (&optional n)
   "Scroll behind N lines (1 by default)."
   (interactive "P")
   (scroll-behind (prefix-numeric-value n)))
 (global-set-key [(M p)] 'scroll-nlines-behind)
 (global-set-key [(M n)] 'scroll-nlines-ahead)
-
-;; C-a: my-beginning-of-line
-; back-to-indentation <-> beginning-of-line
-(defun my-beginning-of-line ()
-  (interactive)
-  (if (bolp)
-      (back-to-indentation)
-    (beginning-of-line)))
-(global-set-key [(C a)] 'my-beginning-of-line)
-
-;; C-e: my-end-of-line
-; 行末に飛ぶ。連続で実行したときは、後ろについている空白とタブを削除
-(defun my-end-of-line ()
-  (interactive)
-  (end-of-line)
-  (if (eq last-command this-command)
-      (delete-horizontal-space)))
-(global-set-key [(C e)] 'my-end-of-line)
 
 ;; %: match-paren
 ; 対応する括弧にジャンプ
@@ -69,5 +46,24 @@
 	(t (self-insert-command (or arg 1))))
   )
 (global-set-key [(%)] 'match-paren)
+
+;;* C-z => 指定した文字にジャンプ
+(defun move-to-char (arg char)
+  "Move to the provided character."
+  (interactive "p\ncMove to char: ")
+    (forward-char 1)
+    (if (search-forward (char-to-string char) nil t arg)
+        t
+      (message "Search failed: \"%c\"" char))
+    (backward-char 1))
+(global-set-key (kbd "C-z") 'move-to-char)
+
+;;* M-z => zap-to-charの改良版
+;;** http://www.emacswiki.org/emacs/ZapUpToChar
+(defadvice zap-to-char (after my-zap-to-char-advice (arg char) activate)
+  "Kill up to the ARG'th occurence of CHAR, and leave CHAR.
+  The CHAR is replaced and the point is put before CHAR."
+  (insert char)
+  (forward-char -1))
 
 ;;; end-of-file
