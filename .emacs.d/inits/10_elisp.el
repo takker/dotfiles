718016ca0c4b92087376849aabf3746b406ab00f
KASHIO Takaaki <t.kashio@gmail.com>
2010-07-31 16:11:44 +0900

    [Emacs] grep_editの設定を追加
    * M-x grep の結果を編集して C-c C-e で編集結果をファイルに反映
    * ToDo: いくつかうまく動かない設定あり。読込専用に関する設定の辺り？

--- .emacs.d/inits/10_elisp.el
+++ .emacs.d/inits/10_elisp.el
@@ -176,3 +176,22 @@
 ;;* auto-save-buffers.el: 全バッファの自動保存
 (require 'auto-save-buffers)
 (run-with-idle-timer 2.0 t 'auto-save-buffers) ; 2秒ごとに自動保存
+
+;;* grep-edit.el: grepの結果を編集
+(require 'grep)
+(require 'grep-edit)
+
+;;** http://d.hatena.ne.jp/rubikitch/20081025/1224869598
+;; (defadvice grep-edit-change-file (around inhibit-read-only activate)
+;;   ""
+;;   (let ((inhibit-read-only t))
+;;     ad-do-it))
+;; ;; (progn (ad-disable-advice 'grep-edit-change-file 'around 'inhibit-read-only) (ad-update 'grep-edit-change-file))
+
+;; (defun my-grep-edit-setup ()
+;;   (define-key grep-mode-map '[up] nil)
+;;   (define-key grep-mode-map "\C-c\C-c" 'grep-edit-finish-edit)
+;;   (message (substitute-command-keys "\\[grep-edit-finish-edit] to apply changes."))
+;;   (set (make-local-variable 'inhibit-read-only) t)
+;;   )
+;; (add-hook 'grep-setup-hook 'my-grep-edit-setup t)
