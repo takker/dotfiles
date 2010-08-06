e4653ceda3be2b7aef01dc37ceb4f47f1d4eaf5c
KASHIO Takaaki <t.kashio@gmail.com>
2010-08-06 17:20:52 +0900

    [Emacs] 関数の追加、変更
    * match-paren: % => C-x % に変更
    * C-S-z => 次に指定された文字を逆方向に検索
    * C-; => 前回実行した１文字検索を繰り返す
    * C-S-h => 表示画面の一番上端に移動
    * C-S-m => 表示画面の中央に移動
    * C-S-l => 表示画面の一番下端に移動
    * C-k => ポイントが行の最後にある場合、行全体を削除するadviceを追加
    * M-o   => 現在行の上に新しい行を追加
    * M-S-o => 現在行の下に新しい行を追加

--- .emacs.d/inits/01_util.el
+++ .emacs.d/inits/01_util.el
@@ -6,7 +6,7 @@
   (message "%s" (get-char-property (point) 'face)))
 (global-set-key [(C x)(f)] 'describe-face-at-point)
 
-;; C-c d => 日付挿入
+;;* C-c d => 日付挿入
 (defun my-insert-date ()
   (interactive)
   (insert (format-time-string "%Y-%m-%dT%R:%S+09:00" (current-time))))
@@ -36,7 +36,7 @@
 (global-set-key [(M p)] 'scroll-nlines-behind)
 (global-set-key [(M n)] 'scroll-nlines-ahead)
 
-;; %: match-paren
+;;* C-x % => match-paren
 ; 対応する括弧にジャンプ
 (defun match-paren (arg)
   "Go to the matching paren if on a paren; otherwise insert %."
@@ -45,18 +45,42 @@
 	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
 	(t (self-insert-command (or arg 1))))
   )
-(global-set-key [(%)] 'match-paren)
+(global-set-key [(C x) (%)] 'match-paren)
 
-;;* C-z => 指定した文字にジャンプ
-(defun move-to-char (arg char)
-  "Move to the provided character."
+;; 前回ジャンプした文字と方向を覚えておくための変数
+(defvar last-search-char " ")
+(defvar last-search-direction 'forward)
+
+;;* C-z => 指定した文字(前方)にジャンプ
+(defun move-to-char-forward (arg char)
+  "Move forward to the provided character."
   (interactive "p\ncMove to char: ")
-    (forward-char 1)
-    (if (search-forward (char-to-string char) nil t arg)
-        t
-      (message "Search failed: \"%c\"" char))
-    (backward-char 1))
-(global-set-key (kbd "C-z") 'move-to-char)
+  (forward-char 1)
+  (if (search-forward (string char) nil t arg)
+      (setq last-search-char char
+            last-search-direction 'forward)
+    (message "Search failed: \"%c\"" char))
+  (backward-char 1))
+(global-set-key (kbd "C-z") 'move-to-char-forward)
+
+;;* C-S-z => 指定した文字(後方)にジャンプ
+(defun move-to-char-backward (arg char)
+  "Move backward to the provided character."
+  (interactive "p\ncMove backward to char: ")
+  (if (search-backward (string char) nil t arg)
+      (setq last-search-char char
+            last-search-direction 'backward)
+    (message "Search failed: \"%c\"" char)))
+(global-set-key (kbd "C-S-z") 'move-to-char-backward)
+
+;;* C-; => 前回ジャンプした文字に繰り返しジャンプ
+(defun repeat-move-to-char (arg)
+  "Repeat move-to-char toward the char and the direction last jumped."
+  (interactive "p")
+  (if (eq last-search-direction 'forward)
+      (move-to-char-forward arg last-search-char)
+    (move-to-char-backward arg last-search-char)))
+(global-set-key (kbd "C-;") 'repeat-move-to-char)
 
 ;;* M-z => zap-to-charの改良版
 ;;** http://www.emacswiki.org/emacs/ZapUpToChar
@@ -65,5 +89,41 @@
   The CHAR is replaced and the point is put before CHAR."
   (insert char)
   (forward-char -1))
+
+;;* C-S-h => 現在のウィンドウで表示されている最初の行に移動
+;;* C-S-m => 現在のウィンドウで表示されている中央の行に移動
+;;* C-S-l => 現在のウィンドウで表示されている最後の行に移動
+(global-set-key-fn (kbd "C-S-h") nil (interactive)
+                   (move-to-window-line 0)
+                   (recenter))
+(global-set-key-fn (kbd "C-S-m") nil (interactive)
+                   (move-to-window-line nil)
+                   (recenter))
+(global-set-key-fn (kbd "C-S-l") nil (interactive)
+                   (move-to-window-line -1)
+                   (recenter))
+
+;;* ポイントが行の最後にある場合、行全体をkill-line
+(defadvice kill-line (before kill-whole-line-if-end
+                             (&optional arg))
+  "Kill the line if the point is on the end of line."
+  (if (eolp) (beginning-of-line)))
+(ad-activate 'kill-line)
+
+;;* M-o => 現在行の上に新しい行を追加
+(defadvice open-line (before open-whole-line
+                             (&optional arg))
+  "Insert newline before the line and leave the point."
+  (beginning-of-line 1))
+(ad-activate 'open-line)
+(global-set-key [(M o)] 'open-line)
+
+;;* M-S-o => 現在行の次に新しい行を追加
+(defun open-next-line (&optional arg)
+  "Insert a newline into the next line and leave point before it."
+  (interactive "p")
+  (next-line 1)
+  (open-line arg))
+(global-set-key [(M S o)] 'open-next-line)
 
 ;;; end-of-file
