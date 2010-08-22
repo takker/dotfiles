f70e2edf253be55f0004357c4d503ef38b21f867
KASHIO Takaaki <t.kashio@gmail.com>
2010-08-22 22:37:39 +0900

    [Emacs] キーバインドの変更
    * delete-backward-word: M-h => C-w (リージョン未選択時)
    * anything-buffers+: C-x C-a C-b => C-x C-b
    * <F8>: auto-installのプレフィックスキー

--- .emacs.d/inits/99_global-keys.el
+++ .emacs.d/inits/99_global-keys.el
@@ -1,6 +1,6 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; -*-
 (global-set-key [(M g)] 'goto-line)
-(global-set-key [(M h)] 'backward-kill-word) ; mark-paragraph
+;; (global-set-key [(M h)] 'backward-kill-word) ; mark-paragraph
 (global-set-key [(M k)] 'kill-whole-line) ; kill-sentence
 (global-set-key [(C m)] 'newline-and-indent)
 ; (global-set-key [(C t)] 'dmacro-exec)
@@ -10,8 +10,20 @@
 
 (global-set-key "\C-\\" 'ibus-toggle)   ; C-\ でmozcのトグル
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; ctl-x-map
+;;* C-x C-b => anything-buffers+
+(define-key ctl-x-map [(C b)] 'anything-buffers+)
+
+;;* C-x C-o => 次のウィンドウに移動
+;;* C-x C-p => 前のウィンドウに移動
+(define-key ctl-x-map [(C o)] 'other-window)
+(global-set-key-fn [(C x) (C p)] nil  ;mark-page
+                   (interactive)
+                   (other-window -1))
+;;* C-x o => 連続する空行の削除
+(define-key ctl-x-map [(o)] 'delete-blank-lines)
+
 ;;* C-x C-d => 別のウィンドウでdired
 (define-key ctl-x-map [(C d)] 'ffap-dired-other-window)
 
@@ -43,11 +55,20 @@
 ;;* C-x C-h => help
 (define-key ctl-x-map [(C h)] 'help-command)
 
-;;* C-x C-b => バッファ切り替え
 ;;* C-x C-l => バッファ一覧
-; (define-key ctl-x-map [(C b)] 'iswitchb-buffer)
 (define-key ctl-x-map [(C l)] 'list-buffers) ; downcase-region
 
 ;;* C-x C-@ => 次のエラー位置へジャンプ
 (define-key ctl-x-map [(C @)] 'next-error)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 特殊キー
+;;* <F8>をauto-install用のキーに割り当て
+;;** w => from emacswiki
+;;** u => from url
+;;** g => from gist
+;;** b => batch
+(global-set-key (kbd "<f8> w") 'auto-install-from-emacswiki)
+(global-set-key (kbd "<f8> u") 'auto-install-from-url)
+(global-set-key (kbd "<f8> g") 'auto-install-from-gist)
+(global-set-key (kbd "<f8> b") 'auto-install-batch)
