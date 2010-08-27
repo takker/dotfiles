1024b7fef0dd86606d25fc352c9d74037d8c50cd
KASHIO Takaaki <t.kashio@gmail.com>
2010-08-27 15:35:51 +0900

    [Emacs] テンプレートファイルの追加
    * perl用テンプレートファイル(_.pl)の追加
    * ruby用テンプレートファイル(_.rb)の追加

---
+++ .emacs.d/etc/templates/_.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+
+
+case $0
+when __FILE__
+  # Write command line interface of the implementation
+
+when /spec[^\/]*$/
+  require 'rubygems'
+  require 'spec'
+  # Write spec of the implementation
+
+end
+
